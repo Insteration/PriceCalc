2d5b7cc59a24460af7fba65154df5eb016da2a8f
Artem Karmaz <inste@icloud.com>
2019-02-12 21:05:21 +0200

    second commit

---
+++ PriceCalc/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  PriceCalc
+//
+//  Created by Artem Karmaz on 2/12/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import Foundation
+
+struct Calculate {
+    let price: Float32
+    let weight: Float32
+    
+    func getValue(price: Float32, weight: Float32) -> Float32 {
+        
+        return 0
+    }
+}
